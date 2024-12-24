e1f437af4ab9fc4823d3a63b71d0d9623c001970
Jxuu03 <xol6899@gmail.com>
2024-12-24 20:20:39 +0700

    Update Views.py

--- matplotlibapp/views.py
+++ matplotlibapp/views.py
@@ -15,17 +15,14 @@ import urllib,base64
 # Create your views here.
 def home(request):
       
-    # โหลดข้อมูล
     data = pd.read_csv('matplotlibapp/media/smartphones_cleaned_v6.csv')
 
-    # สร้างกราฟ
-    fig, ax = plt.subplots(figsize=(6, 4))  # กำหนดขนาดกราฟ
-    data["brand_name"].value_counts().plot(kind='bar', ax=ax)  # ใช้ ax ที่กำหนด
+    fig, ax = plt.subplots(figsize=(6, 4)) 
+    data["brand_name"].value_counts().plot(kind='bar', ax=ax)  
     ax.set_title("brand_name Distribution")
     ax.set_xlabel('Category')
     ax.set_ylabel('Count')
 
-    # แปลงกราฟเป็น Base64
     buf = io.BytesIO()
     fig.savefig(buf, format="png")
     buf.seek(0)
@@ -34,3 +31,34 @@ def home(request):
     buf.close()
 
     return render(request, 'matplotlibapp/graph.html', {'data': uri})
+
+def top_5(request):
+    data = pd.read_csv('matplotlibapp/media/smartphones_cleaned_v6.csv')
+    top_5_brands = data['brand_name'].value_counts().head(5).reset_index()
+    top_5_brands.columns = ['brand_name', 'count']  
+
+
+    fig = plt.bar(top_5_brands, x='brand_name', y='count', title='Top 5 Smartphone Brands', 
+                labels={'count': 'Number of Phones', 'brand_name': 'Brand Name'})
+
+    # fig.show()
+    pass
+
+def top_5(request):
+    data = pd.read_csv('matplotlibapp/media/smartphones_cleaned_v6.csv')
+
+    top_5 = data["brand_name"].value_counts().head(5)
+
+    fig, ax = plt.subplots(figsize=(6, 4))
+    top_5.plot(kind='bar', ax=ax, color='skyblue', edgecolor='black')
+    ax.set_title("Top 5 Models")
+    ax.set_xlabel('Model')
+    ax.set_ylabel('Count')
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format="png")
+    buf.seek(0)
+    graph_data = base64.b64encode(buf.read()).decode()
+    buf.close()
+
+    return render(request, 'matplotlibapp/top_5.html', {'data': graph_data, 'top_5': top_5.to_dict()})
